392131a83b69bc41a92ac38391776b003ccaf6f4
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2014-01-24 15:30:46 -0600

    Now throws error for incorrect schema-definer function argument.

--- test/deliver_tests.js
+++ test/deliver_tests.js
@@ -5,6 +5,22 @@ var odm = require('../lib/stork')
 	;
 
 exports['test deliver fails with non-string first argument'] = function(t) {
-	odm.deliver.should.throw('deliver requires type name');
+	var otherArgs = [null, 0, true, [1, 2, 3], {}, arguments[arguments.length]]
+		;
+
+	otherArgs.forEach(function(arg) {
+		odm.deliver.bind(null, arg).should.throw('deliver requires type name as its first argument');
+	});
+	t.done();
+};
+
+exports['test deliver fails with non-function second argument'] = function(t) {
+	var message = 'deliver requires a schema-defining function as its second argument'
+		,	otherArgs = [null, 0, 'hello', true, [1, 2, 3], {}]
+		;
+
+	otherArgs.forEach(function(arg) {
+		odm.deliver.bind(null, 'name', arg).should.throw(message);
+	});
 	t.done();
 };
