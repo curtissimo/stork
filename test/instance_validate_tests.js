955b3a2be5aa633e463a8e0dbafd075e72cd236b
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2014-01-24 20:42:38 -0600

    Got the validation test thing done.

--- test/instance_validate_tests.js
+++ test/instance_validate_tests.js
@@ -17,6 +17,32 @@ exports['instance has #validate'] = {
 		user.validate.should.be.a.Function;
 		user.validate.should.not.throw();
 		user.validate().should.have.property('valid');
+		user.validate().should.not.have.property('errors');
+		t.done();
+	}
+, 'and schema violations return invalid': function(t) {
+		var user = this.User.new()
+		  , validation
+		  , error
+		  ;
+		user['$schema'].properties = {
+			name: {
+				type: 'string',
+				required: true
+			}
+		}
+		validation = user.validate();
+		validation.should.have.property('valid');
+		validation.should.have.property('errors');
+		validation.valid.should.be.false;
+		validation.errors.should.have.length(1);
+		
+		error = validation.errors[0];
+		error.property.should.be.equal('name');
+		error.attribute.should.be.equal('required');
+		error.expected.should.be.true;
+		(typeof error.actual).should.be.equal('undefined');
+		error.message.should.be.equal('is required');
 		t.done();
 	}
 };
