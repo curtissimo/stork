248104420a22d90dd3ce873edf864fba2a5b39fe
Curtis Schlak <realistschuckle@gmail.com>
2014-08-24 17:40:24 -0500

    stork now finds nested ISO date properties and converts them to dates.

--- test/entity_from_all_tests.js
+++ test/entity_from_all_tests.js
@@ -110,4 +110,44 @@ exports['Entity#from has #all'] = {
       t.done();
     });
   }
+
+, 'that converts nested date time values from strings to Dates': function (t) {
+    var Entity = odm.deliver(this.entityName, function () {
+          this.object('nested', function () {
+            this.datetime('dt');
+          });
+        })
+      , obj1 = {
+          id: 'obj1'
+        , key: 'obj1'
+        , value: null
+        , doc: {
+            nested: {
+              dt: "2012-07-14T05:00:00.000Z"
+            }
+          , kind: this.entityName
+          , _id: 'obj1'
+          , _rev: '1-sdflkj'
+          }
+        }
+      , match = util.clone(obj1.doc)
+      , value = {rows: [ obj1 ]}
+      , db = this.mockDb(null, value)
+      ;
+    match.nested.dt = new Date(match.nested.dt);
+    Entity.from(db).all(function(err, result) {
+      var instance
+        ;
+      should(err).not.be.ok;
+      result.should.be.an.Array;
+      result.should.not.be.empty;
+      result.should.have.length(value.rows.length);
+      instance = result[0];
+
+      instance.should.be.ok;
+      instance.nested.dt.should.be.ok;
+      instance.nested.dt.should.be.equal(match.nested.dt);
+      t.done();
+    });
+  }
 };
